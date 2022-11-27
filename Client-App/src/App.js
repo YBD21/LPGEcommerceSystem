73b7e3b66f7cdf67cb4cbc7c08ed8423249d3299
Ark <ybd@localhost.localdomain>
2022-11-27 11:49:54 +0545

    initial login commit

--- Client-App/src/App.js
+++ Client-App/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./component/Login_Page/Login";
+
+
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
