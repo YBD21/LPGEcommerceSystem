2d0ffb38157a01bb65f714ea4600ba18357d4554
YBD21 <santosh.deuja2000@gmail.com>
2023-01-23 13:52:57 +0545

    CreateLPGProduct Complete : Backend and Client

--- Client-App/src/component/Main_Menu_Page/Admin/Error_Handeling_Message/ErrorMessageBoxAdmin.jsx
+++ Client-App/src/component/Main_Menu_Page/Admin/Error_Handeling_Message/ErrorMessageBoxAdmin.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const ErrorMessageBoxAdmin = () => {
+const ErrorMessageBoxAdmin = (message) => {
+   const [isClicked, setIsClicked] = useState(message.status);
+   const [text, setText] = useState({});
+     useEffect(() => {
+   
+    switch (message.Error_message) {
+      case "Network Error":
+        return setText({
+          First: "Network Error !",
+          Second: "Cannot access to the internet.",
+        });
+
+      default:
+        return setText({
+          First: "Holy smokes !",
+          Second: "Something seriously bad happened.",
+        });
+    }
+  }, [isClicked]);
+
+  const checkClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <div>ErrorMessageBoxAdmin</div>
-  )
+        <>
+      {isClicked && (
+        <div
+          className="flex flex-col justify-center bg-red-100 border border-red-900 text-red-800 px-4 py-3 rounded relative mb-4"
+          role="alert"
+        >
+          {/* Display Error Message */}
+          <div className="m-auto">
+            <strong className="font-bold mr-1">{text.First}</strong>
+            <span className="sm:inline text-center">{text.Second}</span>
+          </div>
+
+          <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg
+              className="fill-current h-6 w-6 text-red-700"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              onClick={checkClick}
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ErrorMessageBoxAdmin
--- Client-App/src/component/Main_Menu_Page/Admin/Manage_Product/CreateLPGasProduct.jsx
+++ Client-App/src/component/Main_Menu_Page/Admin/Manage_Product/CreateLPGasProduct.jsx
@@ -1,50 +1,110 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import SuccessMessageAdmin from "../Success_Message/SuccessMessageBox";
 import ErrorTextMessageAdmin from "../Error_Handeling_Message/ErrorTextMessageAdmin";
 import axios from "axios";
+import ErrorMessageBoxAdmin from "../Error_Handeling_Message/ErrorMessageBoxAdmin";
 
 function CreateLPGProduct() {
   const [image, setImage] = useState(null);
-  const [imageUrl,setImageUrl] = useState('');
-  
-  const [productName,setProductName] = useState('');
-  const [qty,setQty] = useState(0);
-  
-  const [errorProductName,setErrorProductName] = useState({});
-  const [errorOnQty,setErrorOnQty] = useState({});
-  const [errorOnImage,setErrorOnImage] = useState({});
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [productName, setProductName] = useState("");
+  const [qty, setQty] = useState(0);
+
+  const [errorProductName, setErrorProductName] = useState({});
+  const [errorOnQty, setErrorOnQty] = useState({});
+  const [errorOnImage, setErrorOnImage] = useState({});
+  const [errorMessageBox, setErrorMessageBox] = useState(null);
 
   const [success, setSuccess] = useState(null);
 
-  const checkName = () => {
-    let count = 0;
-    
-    if (productName.length <=3 && productName.length !==0){
-        // send an error message
-    }
 
+  useEffect( () => {
+    // erase error message whenever user is typing
+    setErrorProductName({});
+    setErrorOnQty({});
+    setErrorOnImage({});
+    setSuccess(null);
+    
+  }, [image,qty,productName]);
+
+
+  //check image
+  const checkImage = () => {
+   let status = false;
+   let testFormData = new FormData();
+
+   try {
+    testFormData.append("img", image.img);
+   } catch (error) {
+    status = true;
+    setErrorOnImage({
+      Image : true,
+      Message : "Please Upload an Image."
+    });
+   }
+   
+   return status
+  }; 
+
+
+  //check Quantity
+   const checkQuantity = () => {
+     let status = false;
+
+     if (qty < 0){
+      // number cannot be nagative
+      setErrorOnQty({
+        Qty:true,
+        Message : 
+        "Please enter a valid quantity, it should be greater than zero."
+      });
+      status = true;
+     }
+     return status
+   };
+
+  //check Product Name
+  const checkProductName = () => {
+    let status = false;
+
+    if (productName.length >= 0) {
+      if (productName.length <= 3 || productName.length >= 70) {
+        // send an error message
+        setErrorProductName({
+          ProductName: true,
+          Message:
+          "Please enter a product name that is between 3-70 characters long."
+        });
+        status = true;
+      }
+    }
+    return status
   };
+
 
   const handleChange = (e) => {
     if (e.target.files[0]) {
-        let reader = new FileReader();
-        reader.onloadend = () => {
-            setImage({
-                img: e.target.files[0],
-                data: reader.result,
-            });
-            setImageUrl(reader.result);
-        };
-        reader.readAsDataURL(e.target.files[0]);
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        setImage({
+          img: e.target.files[0],
+          data: reader.result,
+        });
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
     }
-};
-
+  };
 
   const uploadImage = () => {
-    let data = {productName : productName, Qty : qty};
+
+    let data = { productName: productName, Qty: qty };
     let formData = new FormData();
+     
     formData.append("img", image.img);
-    formData.append("data",JSON.stringify(data));
+    formData.append("data", JSON.stringify(data));
+
     axios
       .post("http://localhost:5000/uploadData", formData, {
         headers: {
@@ -53,43 +113,68 @@ function CreateLPGProduct() {
       })
       .then((response) => {
         // console.log(response.data);
-        if (response.data.Message === true){
+        if (response.data.Message === true) {
           setSuccess("Success");
+        } else {
+          // set respond message here
         }
       })
       .catch((error) => {
-        // console.log(error);
-        setError(error.message);
+        // console.log(error.message);
+        setErrorMessageBox(error.message);
       });
-    
-     
   };
-  
+
+  const submitForm = () => {
+    let statusImage = false;
+    let statusName = false;
+    let statusQty = false;
+    
+    statusImage = checkImage();
+    statusName = checkProductName();
+    statusQty = checkQuantity();
+
+    if (statusImage && statusName && statusQty) {
+        return;
+    } else {
+        uploadImage();
+    }
+};
+
 
   return (
     <div className="flex flex-col mx-2">
-      <strong className="w-full text-center text-2xl p-3">Create LPG Product</strong>
+      <strong className="w-full text-center text-2xl p-3">
+        Create LPG Product
+      </strong>
       <div className="flex flex-col my-5">
         <label className="block text-sm font-semibold text-gray-800">
           Product Image
         </label>
-        
-        {imageUrl && 
-        <img src={imageUrl} className="ml-2 my-3 w-48 h-48 rounded-lg outline cursor-pointer" alt="Uploaded Image" />
-        
-        }
-        <div className="">
+
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            className="ml-2 my-3 w-32 h-58 rounded-lg outline cursor-pointer"
+            alt="Uploaded Image"
+          />
+        )}
+        <div
+          className="w-full px-5 py-2.5 tracking-wide 
+      text-black font-medium text-s"
+        >
           {/* only accept image */}
           <input
             type="file"
             onChange={handleChange}
             accept="image/*"
-            className="w-full px-5 py-2.5 tracking-wide 
-      text-black font-medium text-s text-center"
+            className="px-5 py-2.5"
           />
         </div>
         {/* Error Message For Image */}
-        {errorOnImage.Image && <ErrorTextMessageAdmin props = {errorOnImage.Message}/>}
+        {errorOnImage.Image && (
+          <ErrorTextMessageAdmin props={errorOnImage.Message} />
+        )}
       </div>
 
       <div className="mb-5">
@@ -102,10 +187,14 @@ function CreateLPGProduct() {
             placeholder="Enter Product Name"
             className="block w-full px-4 py-2 mt-2 text-black-700 border-2 border-black bg-white rounded-md 
             focus:border-black focus:ring-black focus:outline-none focus:ring focus:ring-opacity-40 text-center"
-            value = {productName}
+            value={productName}
             onChange={(e) => setProductName(e.target.value)}
           />
         </div>
+        {/* Error Message For Product Name */}
+        {errorProductName.ProductName && (
+          <ErrorTextMessageAdmin props={errorProductName.Message} />
+        )}
       </div>
 
       <div className="mb-5">
@@ -119,18 +208,25 @@ function CreateLPGProduct() {
             className="block w-full px-4 py-2 mt-2 text-black-700 border-2 border-black bg-white rounded-md focus:border-black
              focus:ring-black focus:outline-none focus:ring focus:ring-opacity-40 text-center"
             value={qty}
-            onChange={(e) => setQty(e.target.value)} 
+            onChange={(e) => setQty(e.target.value)}
           />
         </div>
+        {/* Error Message For Quantity */}
+        {errorOnQty.Qty && (
+          <ErrorTextMessageAdmin props={errorOnQty.Message} />
+        )}
       </div>
       {/* Success Message */}
-      {success && <SuccessMessageAdmin props = {success} status = {true}/> }
-
+      {success && <SuccessMessageAdmin props={success} status={true} />}
+      {/* Error Message */}
+      {errorMessageBox && (
+        <ErrorMessageBoxAdmin Error_message={errorMessageBox} status={true} />
+      )}
       <button
         className="w-full px-5 py-2.5 tracking-wide
             text-white bg-black font-medium rounded-lg text-s 
             text-center mr-3"
-        onClick={uploadImage}
+        onClick={submitForm}
       >
         Create Product
       </button>
