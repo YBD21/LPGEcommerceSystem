4a5babe1962c3bf48f4e4e4ac92ec67c05bf0c6e
YBD21 <santosh.deuja2000@gmail.com>
2023-03-25 09:48:31 +0545

    fixed ui bug in payment popup --client

--- Client-App/src/component/Main_Menu_Page/Payment.jsx
+++ Client-App/src/component/Main_Menu_Page/Payment.jsx
@@ -173,7 +173,7 @@ const Payment = ({ timer }) => {
   };
 
   return (
-    <div className="fixed inset-0 overflow-y-auto z-10">
+    <div className="fixed inset-0 overflow-y-auto z-50">
       <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
