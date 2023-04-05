6a4488c33f0beab62f06f0f2b59cf9dd74c1f8d9
YBD21 <santosh.deuja2000@gmail.com>
2023-04-05 09:51:11 +0545

    updated token

--- Client-App/src/instance.js
+++ Client-App/src/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const url = "http://localhost:5000"
-// export const url = "https://final-year-project-e8d9.onrender.com";
+export const url = "http://localhost:5000";
+// export const url = "https://final-year-project-3eyh.onrender.com";
 const instance = axios.create({
   baseURL: url,
 });
