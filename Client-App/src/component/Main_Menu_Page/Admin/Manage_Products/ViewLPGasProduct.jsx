7453735d11b9b0c89b3adec178daaad32ba96be2
YBD21 <santosh.deuja2000@gmail.com>
2023-04-17 20:01:28 +0545

    fixed Product not updating Admin --client

--- Client-App/src/component/Main_Menu_Page/Admin/Manage_Products/ViewLPGasProduct.jsx
+++ Client-App/src/component/Main_Menu_Page/Admin/Manage_Products/ViewLPGasProduct.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GasProduct from "./GasProduct";
 import { useStateValue } from "../../../../ContextAPI/StateProvider";
 
 const ViewLPGasProduct = () => {
   const [{ productList }] = useStateValue();
+  const [productData, setProductData] = useState({});
+
+  useEffect(() => {
+    setProductData(productList);
+  }, [productList]);
   return (
     <div className="flex-1 p-6 bg-white">
       <h2 className="text-3xl font-bold mt-4 ml-2 mb-8"> Gas Product</h2>
       <div className="w-full flex flex-col">
-      {!productList
+        {!productData
           ? false
-          : Object.entries(productList).map(([key, data]) => (
+          : Object.entries(productData).map(([key, data]) => (
               <GasProduct
                 key={data.Key}
-                keyName = {data.Key}
+                keyName={data.Key}
                 name={data.ProductName}
                 image={data.imageUrl}
                 stock={data.Stock}
