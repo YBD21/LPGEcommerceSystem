2a4f63f6479374c5e62da2e9269087e5a56cbdfc
YBD21 <santosh.deuja2000@gmail.com>
2023-04-26 11:34:21 +0545

    attemping ViewUserList  002 --client --backend

--- Client-App/src/component/Main_Menu_Page/Admin/Manage_Users/ViewUserList.jsx
+++ Client-App/src/component/Main_Menu_Page/Admin/Manage_Users/ViewUserList.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import instance from "../../../../instance";
-
+import EditIcon from "@mui/icons-material/Edit";
+import Loading from "../../../Loading";
 const ViewUserList = () => {
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true); // to testing set false
+  const [userList, setUserList] = useState({});
   const itemsPerPage = 15;
 
   const handleClick = (event) => {
@@ -15,33 +18,111 @@ const ViewUserList = () => {
         withCredentials: true, // enable sending and receiving cookies
       })
       .then((respond) => {
-        console.log(respond);
+        // console.log(respond.data);
+        setUserList(respond.data);
       })
       .catch((error) => {
         console.log(error.message);
       });
   };
 
-  const tableRows = [];
+  useEffect(() => {
+    setIsLoading(true);
+    getData();
+    setIsLoading(false);
+  }, [isLoading]);
 
-  for (let i = (page - 1) * itemsPerPage; i < page * itemsPerPage; i++) {
-    tableRows.push(
-      <tr key={i}>
-        <td className="border px-4 py-2.5 font-bold">{i + 1}</td>
-        <td className="border px-4 py-2">Santosh Deuja</td>
-        <td className="border px-4 py-2">977-9860694050</td>
-        <td className="border px-4 py-2 text-orange-700 font-bold">Admin</td>
-        <td className="border px-4 py-2 font-bold">
-          {"Mon Apr 17 2023 09:20:35 GMT+0545 (Nepal Time)"}
+  const handelEdit = (userData) => {
+    // should open popUp window
+  };
+
+  // count total number users
+  const getTotalOrderCount = () => {
+    let totalOrderCount = 0;
+    Object.keys(userList).forEach((countryCode) => {
+      Object.keys(userList[countryCode]).forEach((phoneNumber) => {
+        totalOrderCount++;
+      });
+    });
+
+    return totalOrderCount;
+  };
+
+  useEffect(() => {
+    const totalCount = getTotalOrderCount();
+    setOrderCount(totalCount);
+    setPage(1);
+  }, [userList]);
+
+  const formatDateTime = (timestamp) => {
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+
+    const date = new Date(timestamp);
+    const dateString = date.toLocaleDateString("en-US", options); // format date and time
+    return dateString;
+  };
+
+  // Get all users as an array
+  const users = Object.keys(userList).flatMap((countryCode) =>
+    Object.keys(userList[countryCode]).flatMap((phoneNumber) => {
+      const user = userList[countryCode][phoneNumber];
+      // console.log(user);
+      return {
+        countryCode,
+        phoneNumber,
+        created: user.Created,
+        status: user.IsDisable ? "Disable" : "-",
+        lastSeen: user.lastSeen,
+        accountType: user.AccountType,
+      };
+    })
+  );
+
+  // console.log(users);
+
+  const tableRows = users.map((user, index) => {
+    const firstName = userList[user.countryCode][user.phoneNumber]["FirstName"];
+    const lastName = userList[user.countryCode][user.phoneNumber]["LastName"];
+    const fullName = firstName + " " + lastName;
+    const fullPhoneNumber = user.countryCode + "-" + user.phoneNumber;
+
+    const created = formatDateTime(user.created);
+    const lastSeen = formatDateTime(user.lastSeen);
+
+    // console.log(fullName);
+    return (
+      <tr key={index}>
+        <td className="border px-4 py-2.5 font-bold">{index + 1}</td>
+        <td className="border px-4 py-2">{fullName}</td>
+        <td className="border px-4 py-2">{fullPhoneNumber}</td>
+        <td className="border px-4 py-2 text-orange-700 font-bold">
+          {user.accountType}
         </td>
+        <td className="border px-4 py-2 font-bold">{created}</td>
+        <td className="border px-4 py-2 font-bold">{lastSeen}</td>
+        <td className="border px-4 py-2 font-bold">{user.status}</td>
         <td className="border px-4 py-2 font-bold">
-          {"Tue Apr 25 2023 13:25:16 GMT+0545 (Nepal Time)"}
+          <button
+            className="py-2 px-3.5 bg-black mr-3 rounded-lg group relative"
+            onClick={() => handelEdit(user)}
+          >
+            {/* Edit */}
+            <EditIcon className="scale-125 text-white pointer-events-none" />
+            <div className="absolute bottom-5 right-8 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 max-sm:group-hover:opacity-0 transition-opacity duration-300 bg-black py-1 px-2 rounded-md">
+              <span className="text-white font-semibold"> Edit </span>
+            </div>
+          </button>
         </td>
-        <td className="border px-4 py-2 font-bold">{"-"}</td>
-        <td className="border px-4 py-2 font-bold">View More</td>
       </tr>
     );
-  }
+  });
 
   const pageNumbers = [];
 
@@ -62,14 +143,21 @@ const ViewUserList = () => {
     );
   }
 
+  if (isLoading) {
+    return <Loading />; // render loading component when isLoading is true
+  }
+
   return (
     <div className="flex-1 bg-white p-2">
       <h2 className="text-3xl font-bold mt-4 ml-2 mb-8">User Listing</h2>
       <button
-        className="px-5 py-2.5 bg-gray-200 rounded-2xl m-2"
+        className="px-5 py-2.5 tracking-wide bg-black 
+        rounded-lg text-center mr-2 mb-2 focus:outline-none 
+        focus:ring-2 focus:ring-black focus:ring-opacity-50 
+        active:ring-4 active:ring-black active:ring-opacity-50"
         onClick={getData}
       >
-        get data{" "}
+        <p className="text-white">Refresh</p>
       </button>
       <table className="table-auto w-full">
         <thead>
